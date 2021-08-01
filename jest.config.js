d18eca5ae416be9d7e2057d88dcd287f6a22c897
Saif Ali Shaik <saifshine7@gmail.com>
2021-08-01 13:17:15 +0530

    tjs-jest-18

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,19 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '\\.module\\.css$': 'identity-obj-proxy',
-    '\\.css$': require.resolve('./test/style-mock.js'),
+  ...require('./test/jest-common'),
+  collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 15,
+      branches: 10,
+      functions: 15,
+      lines: 15,
+    },
+    './src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      functions: 100,
+      lines: 100,
+    },
   },
-  snapshotSerializers: ['jest-emotion'],
+  projects: ['./test/jest.client.js', './test/jest.server.js'],
 }
